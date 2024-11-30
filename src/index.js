6bb81d1132d78deeb47e6a1ac6f0009586677d46
jonathan favorel <favorel.jonathan@outlook.fr>
2024-11-30 15:34:13 +0100

    Mise à jour des statistiques de trading pour inclure les types de trades et les calculs de profit/perte réels

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
-const tradingAccountRoutes = require('./routes/tradingAccountRoutes');
-const propFirmRoutes = require('./routes/propFirmRoutes');
-const currencyRoutes = require('./routes/currencyRoutes');
-const tradeRoutes = require('./routes/tradeRoutes');
-const friendRoutes = require('./routes/friendRoutes');
-const enemyRoutes = require('./routes/enemyRoutes');
-const messageRoutes = require('./routes/messageRoutes');
-const dotenv = require('dotenv');
+const express = require("express");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const tradingAccountRoutes = require("./routes/tradingAccountRoutes");
+const propFirmRoutes = require("./routes/propFirmRoutes");
+const currencyRoutes = require("./routes/currencyRoutes");
+const tradeRoutes = require("./routes/tradeRoutes");
+const friendRoutes = require("./routes/friendRoutes");
+const enemyRoutes = require("./routes/enemyRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const dotenv = require("dotenv");
+const initDB = require("./initDB");
+const initPropFirms = require("./initPropFirms");
 
 dotenv.config();
 
@@ -18,15 +20,19 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.use('/api/users', userRoutes);
-app.use('/api/tradingaccounts', tradingAccountRoutes);
-app.use('/api/propfirms', propFirmRoutes);
-app.use('/api/currencies', currencyRoutes);
-app.use('/api/trades', tradeRoutes);
-app.use('/api/friends', friendRoutes);
-app.use('/api/enemies', enemyRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/tradingaccounts", tradingAccountRoutes);
+app.use("/api/propfirms", propFirmRoutes);
+app.use("/api/currencies", currencyRoutes);
+app.use("/api/trades", tradeRoutes);
+app.use("/api/friends", friendRoutes);
+app.use("/api/enemies", enemyRoutes);
+app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, async () => {
+  console.log(`Server started on port ${PORT}`);
+  await initDB();
+  await initPropFirms();
+});
